9fac57f6d20da804e2d264d98225241f95b3f262
cloner54 <cloner5477@gmail.com>
2023-04-16 20:18:15 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='0262ab80-d987-11ed-8388-1239d0255272'
 VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
+VLESS_WSPATH='/vlessme'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER=''
